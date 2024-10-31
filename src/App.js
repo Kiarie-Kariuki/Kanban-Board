02fb7d69a4b4895df885d24e30988aa76af66b72
Kiarie <collinskiarie13@outlook.com>
2024-10-31 14:12:29 +0300

    Added further routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { UserProvider } from './Components/UserContext';
 import Navbar from './Components/Navbar';
 import SignUp from './Components/Sign-up/SignUp';
 import SignIn from './Components/sign-in/SignIn';
+import CustomBoard from './Components/CustomBoard';
 
 function App() {
   return (
-    <Router>
-    <Navbar />
-    <Routes>
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/signin" element={<SignIn />} />
-    </Routes>
-  </Router>
+    <UserProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<SignUp />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/board" element={<CustomBoard />} />
+          </Routes>
+        </Router>
+    </UserProvider>
   );
 }
 
