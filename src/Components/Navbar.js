260ba869dab45413a8cefa95c208d41d6a35cbe4
Kiarie <collinskiarie13@outlook.com>
2024-10-31 14:10:43 +0300

    Added the logic for the nav icon during signing in and out

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,20 +1,43 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link , useNavigate} from 'react-router-dom';
+import { useUser } from './UserContext';
 
 const Navbar = () => {
+  const { user, signOut } = useUser();
+
+  const navigate = useNavigate();
+
+
+
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" style={{ flexGrow: 1 }}>
           Kanban Board
         </Typography>
-        <Button color="inherit" component={Link} to="/signup">
-          Sign Up
-        </Button>
-        <Button color="inherit" style={{ marginRight: '80px' }} component={Link} to="/signin">
-          Sign In
-        </Button>
+        {user ? ( 
+        <>
+          <Typography variant="body1" style={{ marginRight: '20px' }}>
+              Hello, {user.fullname}
+            </Typography>
+            <Button color="inherit" onClick={() => {
+              signOut();
+              navigate('/signin'); 
+            }}>
+              Sign Out
+            </Button>
+        </>
+      ) : (
+        <>
+          <Button color="inherit" component={Link} to="/signup">
+                Sign Up
+          </Button>
+          <Button color="inherit" style={{ marginRight: '80px' }} component={Link} to="/signin">
+            Sign In
+          </Button>
+        </>
+      )}
       </Toolbar>
     </AppBar>
   );
