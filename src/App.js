970864eca9364920fbec25e469381bd42cd3d5b5
Kiarie <collinskiarie13@outlook.com>
2026-05-05 11:15:55 +0300

    Imported other components and created routes to access all these pages

--- src/App.js
+++ src/App.js
@@ -5,19 +5,28 @@ import Navbar from './Components/Navbar';
 import SignUp from './Components/Sign-up/SignUp';
 import SignIn from './Components/sign-in/SignIn';
 import CustomBoard from './Components/CustomBoard';
+import Profile from './Components/Profile';
+import Students from './Components/Students';
+import StudentProfile from './Components/StudentProfile';
+import AppTheme from './Components/shared-theme/AppTheme';
 
 function App() {
   return (
-    <UserProvider>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<SignUp />} />
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/board" element={<CustomBoard />} />
-          </Routes>
-        </Router>
-    </UserProvider>
+    <AppTheme>
+      <UserProvider>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<SignUp />} />
+              <Route path="/signin" element={<SignIn />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/board" element={<CustomBoard />} />
+              <Route path="/students" element={<Students />} />
+              <Route path="/students/:id" element={<StudentProfile />} />
+            </Routes>
+          </Router>
+      </UserProvider>
+    </AppTheme>
   );
 }
 
